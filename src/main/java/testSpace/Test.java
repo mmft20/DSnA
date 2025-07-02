9505e1ef363fab2d52a57ab1988003e8de93991c
Rajat Dhingra <rajatdhingra_rd@yahoo.com>
2025-07-03 07:33:11 +0800

    DSnA : Test Class : Additional sysout statement added

--- src/main/java/testSpace/Test.java
+++ src/main/java/testSpace/Test.java
@@ -3,5 +3,6 @@ package testSpace;
 public class Test {
     public static void main(String[] args) {
         System.out.println("This is a temporary workspace class, where experimentation code goes.");
+        System.out.println("After completing the experimental code, ensure that the code is deleted from this workspace.");
     }
 }
