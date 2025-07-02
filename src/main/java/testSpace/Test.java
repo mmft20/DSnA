66de255d7d2bc42ab36968668003b20ff4bd7353
Rajat Dhingra <rajatdhingra_rd@yahoo.com>
2025-07-02 08:11:45 +0800

    DSnA : First Commit testSpace package Test Class for experimentalCode

---
+++ src/main/java/testSpace/Test.java
@@ -0,0 +1,7 @@
+package testSpace;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("This is a temporary workspace class, where experimentation code goes.");
+    }
+}
